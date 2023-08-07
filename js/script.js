e47635f118dd031cce2cad2db0f3c6e95ab53a3a
adrian basal <basaladrian@gmail.com>
2023-08-06 22:10:45 -0300

    Actualziacion - 22.10pm

--- js/script.js
+++ js/script.js
@@ -2,6 +2,29 @@
 //Mensaje de bienvenida 
 alert("Bienvenido/a a Santero Café. \n*Recuerda que la reserva mínima es para 2 personas")
 
+function hacerReserva(nombreCliente, numPersonas) {
+	if (verificarDisponibilidad(numPersonas)) {
+	  alert(`La reserva a nombre de ${nombreCliente} ha sido exitosa! \nSe han reservado ${numPersonas} lugares a las ${horario} Hrs.`);
+	  
+	} else {
+	  alert(`Lo siento, no hay mesas disponibles para ${numPersonas} persona.`);
+	}
+  }
+  //Ingresar nombre
+  const nombreCliente = prompt("Ingrese su nombre:");
+
+//Verificar cantidad de lugares
+function verificarDisponibilidad(numPersonas) {
+	if (numPersonas >= 2) {
+	  return true;
+	} else {
+	  return false;
+	}
+  }
+ 
+  //Ingresar cantidad personas
+  const numMesasDeseadas = parseInt(prompt("Cuántos lugares desea reservar?"));
+
 //Elegir horario
 let horario = prompt("Selecciona un horario para la reserva. \nOpción 1 - 21 horas \nOpción 2 - 22 horas \nOpción 3 - 23 horas \n\nPresiona (X) para salir.");
 while (horario != "x") {
@@ -24,31 +47,10 @@ default:
 //Condicion de salida	
 alert("Esperamos que vuelvas pronto. Gracias!");
 }
-
-//Ingresar cantidad de lugares
-function verificarDisponibilidad(numPersonas) {
-	if (numPersonas >= 2) {
-	  return true;
-	} else {
-	  return false;
-	}
-  }
-  
-
-  function hacerReserva(nombreCliente, numPersonas) {
-	if (verificarDisponibilidad(numPersonas)) {
-	  alert(`La reserva a nombre de ${nombreCliente} ha sido exitosa! \nSe han reservado ${numPersonas} lugares a las ${horario} Hrs.`);
-	  
-	} else {
-	  alert(`Lo siento, no hay mesas disponibles para ${numPersonas} persona.`);
-	}
-  }
-  
-  const nombreCliente = prompt("Ingrese su nombre:");
-  const numMesasDeseadas = parseInt(prompt("Cuántos lugares desea reservar?"));
   
   hacerReserva(nombreCliente, numMesasDeseadas, horario);
   console.log(hacerReserva)
+  
   alert("Gracias por la reserva, lo esperamos!");
   
 }
